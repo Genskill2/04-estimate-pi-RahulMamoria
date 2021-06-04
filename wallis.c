65c9ca620647f63a939e6bc14dbd6abdc32fa4f9
RahulMamoria <85018957+RahulMamoria@users.noreply.github.com>
2021-06-04 13:04:29 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -1,9 +1,58 @@
-#include <assert.h>
+ #include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 
-float wallis_pi(int);
+float mc_pi(int);
+
+float frandom() {
+  long int q = random();
+  float ret = (float)q/(float)RAND_MAX;
+  return ret;
+}
+
+float mc_pi(int n)
+{  
+  float x,y,pie,z;
+  int count=0;
+  for(int i=0;i<n;i++)
+   {
+      x=frandom();
+     
+      y=frandom();
+      
+      z=x*x+y*y;
+    
+      if(z<=1)
+        count++;
+     }
+   pie=(float)count/n*4.0;
+   
+   return pie;
+ }  
+   
+int main(void) {
+  float pi0;
+  float pi1;
+  
+  pi0 = mc_pi(25000);
+  pi1 = mc_pi(25000);
+  printf("%f %f\n", pi0, pi1);
+  
+  if (pi0 == pi1) {
+      printf("Two separate estimates of pi are exactly the same. This is unlikely.\n");
+      abort();
+    }
+
+  if (fabs(pi0 - pi1) > 0.05) {
+      printf("Two separate estima…
+[12:54 PM, 6/4/2021] Yash Nitc: monte carlo vala
+[12:54 PM, 6/4/2021] Yash Nitc: #include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+float wallis_pi(int n);
 
 int main(void) {
   float pi;
@@ -24,3 +73,15 @@ int main(void) {
   }
 }
 
+float wallis_pi(int n)
+{
+ float pie=1.0;
+ float num;
+ for(int i=1;i<n;i++)
+ {
+   num=4.0*i*i;
+   pie=pie*num/(num-1.0);
+ }
+   pie=pie*2.0;
+   return pie;
+}
